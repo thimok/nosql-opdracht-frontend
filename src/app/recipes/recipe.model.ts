be11529a38390ba0679769e5ccfaf2325199716a
Twan van Maastricht <ajmaastr@avans.nl>
2017-11-27 11:12:20 +0100

    Added _id attribute to recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,6 +1,7 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 export class Recipe {
+  public _id: string;
   public name: string;
   public description: string;
   public imagePath: string;
@@ -9,6 +10,15 @@ export class Recipe {
   constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
+  }
+
+
+  constructor(id: string, name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
+    this._id = id;
+    this.name = name;
+    this.description = description;
     this.imagePath = imagePath;
     this.ingredients = ingredients;
   }
